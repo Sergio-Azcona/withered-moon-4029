c162e5e27a5f80f8b38378912a784d4b1ea11643
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2022-11-15 11:36:24 -0500

    Test/feat: relationships set up

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -3,5 +3,9 @@ require 'rails_helper'
 RSpec.describe Airline, type: :model do
   describe 'relationships' do
     it {should have_many :flights}
+
+    # it { should have_many(:passengers).through:(:flights) }
+    # it { should have_many(:passengers).through(:flight_passengers) }
+
   end
 end
