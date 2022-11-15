e2882d49aa1923212bbc7a9f804b6a7b28d9d6a1
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2022-11-15 13:02:56 -0500

    test/feat: story 2 complete

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 2022_11_15_161959) do
     t.index ["airline_id"], name: "index_flights_on_airline_id"
   end
 
+  create_table "gardens", force: :cascade do |t|
+    t.string "name"
+    t.boolean "organic"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "passengers", force: :cascade do |t|
     t.string "name"
     t.integer "age"
@@ -44,7 +51,37 @@ ActiveRecord::Schema.define(version: 2022_11_15_161959) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "plants", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "days_to_harvest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "plot_plants", force: :cascade do |t|
+    t.bigint "plot_id"
+    t.bigint "plant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["plant_id"], name: "index_plot_plants_on_plant_id"
+    t.index ["plot_id"], name: "index_plot_plants_on_plot_id"
+  end
+
+  create_table "plots", force: :cascade do |t|
+    t.integer "number"
+    t.string "size"
+    t.string "direction"
+    t.bigint "garden_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["garden_id"], name: "index_plots_on_garden_id"
+  end
+
   add_foreign_key "flight_passengers", "flights"
   add_foreign_key "flight_passengers", "passengers"
   add_foreign_key "flights", "airlines"
+  add_foreign_key "plot_plants", "plants"
+  add_foreign_key "plot_plants", "plots"
+  add_foreign_key "plots", "gardens"
 end
